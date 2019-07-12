0c0cba16b9346b71aa949ca5202350febdb84e05
ashu7073 <ashu7073657271@gmail.com>
2019-07-12 19:39:33 +0530

    Revert some Changes

--- device.mk
+++ device.mk
@@ -142,15 +142,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.device@1.0-impl \
-    android.hardware.camera.device@3.2-impl \
-    android.hardware.camera.device@3.3-impl \
-    android.hardware.camera.device@3.4-impl \
-    android.hardware.camera.common@1.0-impl \
-    android.hardware.camera.device@1.0-service \
-    android.hardware.camera.device@3.2-service \
-    android.hardware.camera.device@3.3-service \
-    android.hardware.camera.device@3.4-service \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
     snap
@@ -447,9 +438,6 @@ PRODUCT_PACKAGES += \
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v28/arm/arch-arm-armv7-a-neon/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.frameworks.sensorservice@1.0-v28.so \
-    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/android.frameworks.sensorservice@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.frameworks.sensorservice@1.0-v28.so
     
 # VR
 PRODUCT_PACKAGES += \
@@ -479,7 +467,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Vendor properties
-#-include $(LOCAL_PATH)/vendor_prop.mk
+-include $(LOCAL_PATH)/vendor_prop.mk
 # Wi-Fi Display
 PRODUCT_BOOT_JARS += \
     WfdCommon
