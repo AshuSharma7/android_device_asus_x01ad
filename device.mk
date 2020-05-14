1801a47d38f8605d10d01d06a70454201531db15
Ashu Sharma <ashu7073657271@gmail.com>
2020-05-14 23:55:41 +0530

    Add thermal HALs

--- device.mk
+++ device.mk
@@ -385,6 +385,12 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8953
+
 
 # WiFi
 PRODUCT_COPY_FILES += \
