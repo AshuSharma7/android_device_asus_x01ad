7286bccd964990e4d33c100e345f5a8c73b978b9
Ashu Sharma <ashu7073657271@gmail.com>
2020-05-14 21:46:38 +0530

    Add Screen Density and Bootanimation Properties

--- device.mk
+++ device.mk
@@ -1,3 +1,12 @@
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1520
+TARGET_SCREEN_WIDTH := 720
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
