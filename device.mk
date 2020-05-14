eabd5ff37f7032d4abdc4c2ad1970206fcc0ce65
Ashu Sharma <ashu7073657271@gmail.com>
2020-05-14 22:38:05 +0530

    Add display HALs

--- device.mk
+++ device.mk
@@ -85,6 +85,30 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     Snap
 
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.1-service
+
+# Display
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    gralloc.msm8953 \
+    hwcomposer.msm8953 \
+    memtrack.msm8953 \
+    libdisplayconfig \
+    liboverlay \
+    libqdMetaData \
+    libqdMetaData.system \
+    libgenlock \
+    libtinyxml \
+    vendor.display.config@1.9
+
 
 # GPS
 PRODUCT_COPY_FILES += \
